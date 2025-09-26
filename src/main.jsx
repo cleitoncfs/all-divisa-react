f761094601641807c5e8eeaa74fae0a55e61ca85
cleitoncfs <cleitoncfs@gmail.com>
2025-09-26 17:04:01 +0100

    Add Estrutura de PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((reg) => console.log('Service Worker registrado:', reg))
+      .catch((err) => console.error('Erro ao registrar SW:', err));
+  });
+}
